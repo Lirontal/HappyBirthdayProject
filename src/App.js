bae7803022b87ae67869ab78e2b3275b32d6a30b
Lirontal1 <315852582@idf.il>
2022-10-11 01:04:16 +0300

    finished up to balloons

--- src/App.js
+++ src/App.js
@@ -1,120 +1,136 @@
 import React, { useRef, useEffect } from "react";
-import { gsap } from "gsap";
+import { gsap, Expo } from "gsap";
 import "./App.css"
 import "./temp.css"
+import bal1 from './images/balloon1.svg';
+import bal2 from './images/balloon2.svg';
+import bal3 from './images/balloon3.svg';
 
+import { getRandomInt } from "./utils";
 export default function App() {
-  //const app = useRef();
 
   const el = useRef();
   const q = gsap.utils.selector(el);
   const tl = useRef();
+  const balloons = [bal1, bal2, bal3];
 
   const ideaTextTrans = {
-    opacity: 0,
+    opacity: 1,
     y: -20,
-    rotationX: 5,
-    skewX: "15deg"
+    duration: 0.7,
+
   };
 
   const ideaTextTransLeave = {
     opacity: 0,
     y: 20,
     rotationY: 5,
-    skewX: "-15deg"
+    duration: 0.7,
   };
+
+  const textbox = `אהובה שלי ❤️
+המון המון מזל טוב, אני מאחל לך....`;
+
+  const a = `<span>${textbox
+    .split("")
+    .join("</span><span>")}</span`;
+  const b =
+    textbox.split("").map((c, i) => <span key={i}>{c}</span>)
 
   useEffect(() => {
 
     tl.current = gsap.timeline()
+        .from(q(".text-box"), {
+         visibility: "hidden"
+       })
       .to(q(".container"), {
         duration: 0.1,
         visibility: "visible"
       })
 
-      // .from(q(".one"), {
-      //   duration: 0.4,
-      //   opacity: 0,
-      //   y: 0
-      // })
-      // .from(q(".two"), {
-      //   duration: 0.4,
-      //   opacity: 0,
-      //   y: 10
-      // }, "-=1")
-      // .to(q(".one"), {
-      //   duration: 0.7,
-      //   opacity: 1,
-      //   y: -10
-      // }, "+=0.5")
-      // .to(q(".two"), {
-      //   duration: 0.7,
-      //   opacity: 1,
-      //   y: -10
-      // }, "+=0.5")
-      // .to(q(".one"), {
-      //   duration: 0.7,
-      //   opacity: 0,
-      //   y: 10
-      // }, "+=0.5")
-      // .to(q(".two"), {
-      //   duration: 0.7,
-      //   opacity: 0,
-      //   y: 10
-      // }, "+=0.5")
-
-
-      // .from(q(".three"), {
-      //   duration: 0.4,
-      //   opacity: 0,
-      //   y: 0
-      // })
-      // .to(q(".three"), {
-      //   duration: 0.7,
-      //   opacity: 1,
-      //   y: -10
-      // }, "+=0.5")
-      // .to(q(".three"), {
-      //   duration: 0.7,
-      //   opacity: 0,
-      //   y: 10
-      // }, "+=0.5")
-
-
-
-
-
-      // .from(q(".text-box"), {
-      //         duration: 0.1,
-      //         opacity: 0,
-      //         scale:0.2,
-
-      //       })
-      //       .to(q(".text-box"), {
-      //         duration: 0.7,
-      //         opacity: 1,
-      //         scale:1,
-
-      //       }, "+=0.5")
-
-      .from(q(".four"), {
-        duration: 0.7,
-        scale: 0.2,
-        opacity: 0
-      })
-      .from(q(".fake-btn"),{
-        duration: 0.7,
-        scale: 0.2,
-        opacity: 0
-      })
-      .to(
-        q(".hbd-chatbox"),
-        {
+        .from(q(".one"), {
+          duration: 0.4,
+          opacity: 0,
+          y: 0
+        })
+        .from(q(".two"), {
+          duration: 0.4,
+          opacity: 0,
+          y: 10
+        }, "-=1")
+        .to(q(".one"), {
           duration: 0.7,
-          visibility: "visible"
-        },
-        0.05
-      )
+          opacity: 1,
+          y: -10
+        }, "+=0.5")
+        .to(q(".two"), {
+          duration: 0.7,
+          opacity: 1,
+          y: -10
+        }, "+=0.5")
+        .to(q(".one"), {
+          duration: 0.7,
+          opacity: 0,
+          y: 10
+        }, "+=0.5")
+        .to(q(".two"), {
+          duration: 0.7,
+          opacity: 0,
+          y: 10
+        }, "+=0.5")
+
+
+        .from(q(".three"), {
+          duration: 0.4,
+          opacity: 0,
+          y: 0
+        })
+        .to(q(".three"), {
+          duration: 0.7,
+          opacity: 1,
+          y: -10
+        }, "+=0.5")
+        .to(q(".three"), {
+          duration: 0.7,
+          opacity: 0,
+          y: 10
+        }, "+=0.5")
+
+
+
+
+
+        .from(q(".text-box"), {
+                duration: 2.1,
+                opacity: 0,
+                scale:0.2,
+              })
+              .to(q(".text-box"), {
+                duration: 0.7,
+                opacity: 1,
+                scale:1,
+                visibility: "visible"
+
+              }, "+=0.5")
+
+        .from(q(".fake-btn"), {
+          duration: 0.7,
+          scale: 0.2,
+          opacity: 0
+        })
+        .to(q(".fake-btn"), {
+          duration: 0.7,
+          scale: 1,
+          opacity: 1
+        }, "-=0.5")
+        .to(
+          q(".hbd-chatbox span"),
+          {
+            stagger: 0.08,
+            duration: 0.7,
+            visibility: "visible"
+          },
+        )
       .to(q(".fake-btn"), {
         duration: 0.7,
         backgroundColor: "rgb(127, 206, 248)"
@@ -130,8 +146,135 @@ export default function App() {
         "+=0.7"
       )
 
+
+
+      .from(q(".idea-1"), ideaTextTransLeave)
+      .to(q(".idea-1"), ideaTextTrans, "+=1")
+      .to(q(".idea-1"), {
+        duration: 0.7,
+        opacity: 0,
+        y: 20
+      }, "+=1")
+
+      .from(q(".idea-2"), ideaTextTransLeave)
+      .to(q(".idea-2"), ideaTextTrans, "+=1")
+      .to(q(".idea-2"), {
+        duration: 0.7,
+        opacity: 0,
+        y: 20
+      }, "+=1")
+
+      .from(q(".idea-3"), { ...ideaTextTransLeave, visibility: "hidden" })
+      .to(q(".idea-3"), {
+        ...ideaTextTrans, visibility: "visible"
+      }, "+=1")
+      .to(".idea-3 strong", {
+        duration: 0.5,
+        scale: 1.2,
+        x: -10,
+        backgroundColor: "rgb(21, 161, 237)",
+        color: "#fff",
+        visibility: "visible"
+      })
+      .to(q(".idea-3"), {
+        duration: 0.7,
+        opacity: 0,
+        y: 20
+      }, "+=0.5")
+
+    .from(q(".idea-4"), ideaTextTransLeave)
+    .to(q(".idea-4"), ideaTextTrans, "+=1")
+    .to(q(".idea-4"), {
+      duration: 0.7,
+      opacity: 0,
+      y: 20
+    }, "+=0.5")
+
+
+
+    .from(q(".idea-5"), {
+      ...ideaTextTransLeave, rotationX: 15,
+      rotationZ: -10,
+      skewY: "-5deg",
+      z: 10
+    })
+    .to(q(".idea-5"), {
+      ...ideaTextTrans, rotationX: 15,
+      rotationZ: -10,
+      skewY: "-5deg",
+      z: 10
+    }, "+=1")
+    .to(
+      q(".idea-5 .smiley"),
+      {
+        duration: 0.7,
+        rotation: -90,
+        x: 8
+      },
+      "+=0.4"
+    )
+    .to(q(".idea-5"), {
+      duration: 0.6,
+      opacity: 0,
+      y: 20
+    }, "+=0.3")
+
+    .from(
+      q(".idea-6 span"),
+      {
+        duration: 0.8,
+        opacity: 0,
+        rotation: 15,
+        ease: Expo.easeIn
+      },
+    )
+    .to(q(".idea-6 span"), {
+      stagger: 0.2,
+      scale: 1,
+      opacity: 1,
+      rotation: 0,
+    }, "+=1")
+    .to(
+      q(".idea-6 span"),
+      {
+        stagger: 0.2,
+        duration: 0.8,
+        scale: 1,
+        opacity: 0,
+        rotation: -15,
+        ease: Expo.easeIn
+      },
+      "+=1"
+    )
+
+
+    .fromTo(
+      q(".baloons img"),
+      {
+        stagger: 0.2,
+        opacity: 0.9,
+        y: 1400
+      },
+      {
+        stagger:0.2,
+        opacity: 1,
+        y: -1000,
+        duration: 2.5,
+      },
+    )
+
+
   }, []);
 
+  const randomBalloons = (n = 50) => {
+   // return;
+    const res = [];
+    [...Array(n)].forEach((e, i) => {
+      const randomBalloonIndex = getRandomInt(balloons.length);
+      res.push(<img src={balloons[randomBalloonIndex]} alt="" />)
+    })
+    return res;
+  }
   return (
     <div ref={el} className="App">
       <div className="container">
@@ -147,74 +290,45 @@ export default function App() {
         </div>
         <div className="four">
           <div className="text-box">
-            <p className="hbd-chatbox" data-node-name="textInChatBox">אהובה שלי ❤️
-              <br />
-              המון המון מזל טוב, אני מאחל לך....</p>
-            <p className="fake-btn" data-node-name="sendButtonLabel">שלח</p>
+            <p className="hbd-chatbox" data-node-name="textInChatBox">
+              {b}
+            </p>
+          </div>
+          <p className="fake-btn" data-node-name="sendButtonLabel">שלח</p>
+        </div>
+
+
+        <div className="five">
+          <p className="idea-1" data-node-name="text2">זה מה שהתכוונתי לעשות</p>
+          <p className="idea-2" data-node-name="text3">אבל אז עצרתי</p>
+          <p className="idea-3">
+            <span data-node-name="text4">הבנתי שאני רוצה לעשות משהו</span>
+            <strong data-node-name="text4Adjective">מיוחד</strong></p>
+          <p className="idea-4" data-node-name="text5Entry">בגלל</p>
+          <p className="idea-5">
+            <span className="idea5text" data-node-name="text5Content">שאת מיוחדת</span>
+            <span className="smiley" data-node-name="smiley">:)</span>
+          </p>
+          <p className="idea-6">
+            <span data-node-name="bigTextPart1">א</span>
+            <span data-node-name="bigTextPart2">ז</span>
+          </p>
+        </div>
+
+        <div className="six">
+          <img src="images/reut.JPG" alt="" className="lydia-dp" data-node-name="imagePath" />
+          <img src="images/hat.svg" alt="" className="hat" />
+          <div className="wish">
+            {/* <h3 className="wish-hbd" data-node-name="wishHeading">Happy Birthday!</h3>
+            <h5 data-node-name="wishText">May the js.prototypes always be with you! ;)</h5> */}
           </div>
         </div>
-        {/* <div className="five">
-      <p className="idea-1" data-node-name="text2">That's what I was going to do.</p>
-      <p className="idea-2" data-node-name="text3">But then I stopped.</p>
-      <p className="idea-3">
-        <span data-node-name="text4">I realised, I wanted to do something</span>
-        <strong data-node-name="text4Adjective">special</strong>.</p>
-      <p className="idea-4" data-node-name="text5Entry">Because,</p>
-      <p className="idea-5">
-        <span data-node-name="text5Content">You are Special</span>
-        <span className="smiley" data-node-name="smiley">:)</span>
-      </p>
-      <p className="idea-6">
-        <span data-node-name="bigTextPart1">S</span>
-        <span data-node-name="bigTextPart2">O</span>
-      </p>
-    </div>
-    <div className="six">
-      <img src="img/lydia2.png" alt="" className="lydia-dp" data-node-name="imagePath"/>
-      <img src="img/hat.svg" alt="" className="hat"/>
-      <div className="wish">
-        <h3 className="wish-hbd" data-node-name="wishHeading">Happy Birthday!</h3>
-        <h5 data-node-name="wishText">May the js.prototypes always be with you! ;)</h5>
-      </div>
-    </div>
-    <div className="seven">
-      <div className="baloons">
-        <img src="img/ballon2.svg" alt=""/>
-        <img src="img/ballon1.svg" alt=""/>
-        <img src="img/ballon3.svg" alt=""/>
-        <img src="img/ballon1.svg" alt=""/>
-        <img src="img/ballon2.svg" alt=""/>
-        <img src="img/ballon3.svg" alt=""/>
-        <img src="img/ballon2.svg" alt=""/>
-        <img src="img/ballon3.svg" alt=""/>
-        <img src="img/ballon1.svg" alt=""/>
-        <img src="img/ballon2.svg" alt=""/>
-        <img src="img/ballon3.svg" alt=""/>
-        <img src="img/ballon2.svg" alt=""/>
-        <img src="img/ballon1.svg" alt=""/>
-        <img src="img/ballon3.svg" alt=""/>
-        <img src="img/ballon2.svg" alt=""/>
-        <img src="img/ballon3.svg" alt=""/>
-        <img src="img/ballon1.svg" alt=""/>
-        <img src="img/ballon2.svg" alt=""/>
-        <img src="img/ballon1.svg" alt=""/>
-        <img src="img/ballon3.svg" alt=""/>
-        <img src="img/ballon3.svg" alt=""/>
-        <img src="img/ballon1.svg" alt=""/>
-        <img src="img/ballon2.svg" alt=""/>
-        <img src="img/ballon3.svg" alt=""/>
-        <img src="img/ballon2.svg" alt=""/>
-        <img src="img/ballon1.svg" alt=""/>
-        <img src="img/ballon3.svg" alt=""/>
-        <img src="img/ballon2.svg" alt=""/>
-        <img src="img/ballon3.svg" alt=""/>
-        <img src="img/ballon1.svg" alt=""/>
-        <img src="img/ballon2.svg" alt=""/>
-        <img src="img/ballon1.svg" alt=""/>
-        <img src="img/ballon3.svg" alt=""/>
-      </div>
-    </div>
-    <div className="eight">
+        <div className="seven">
+          <div className="baloons">
+            {randomBalloons()}
+          </div>
+        </div>
+        {/* <div className="eight">
       <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
         <circle cx="20" cy="20" r="20" />
       </svg>
@@ -242,8 +356,8 @@ export default function App() {
       <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
         <circle cx="20" cy="20" r="20" />
       </svg>
-    </div>
-    <div className="nine">
+    </div> */}
+        {/*<div className="nine">
       <p data-node-name="outroText">Okay, now come back and tell me if you liked it.</p>
       <p id="replay" data-node-name="replayText">Or click, if you want to watch it again.</p>
       <p className="last-smile" data-node-name="outroSmiley">:)</p>
